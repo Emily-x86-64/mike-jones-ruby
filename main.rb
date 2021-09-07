914783d5828bdc99bba354744e7af2d10a2f8c00
AkronOhioScott <eastonpalmer0@gmail.com>
2021-09-07 18:00:59 -0400

    Verison 1.0

--- main.rb
+++ main.rb
@@ -1,8 +1,10 @@
-# frozen_string_literal: true
-
 def main
-  8.times do
-    puts "Back then hoes didn't want me, now I'm hot hoes all on me"
+  8.times do |i|
+    if i.even?
+      puts 'Back then hoes didn\'t want me, now I\'m hot hoes all on me'
+    else
+      puts 'Back then hoes didn\'t want me, now I\'m hot hoes all on me,' << ' I said'
+    end
   end
 end
 
